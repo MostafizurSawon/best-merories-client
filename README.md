c3ba3dfd4e8466f96df4d73fde7aea5b0b7df7d1
MostafizurSawon <MostafizurSawon@users.noreply.github.com>
2022-01-11 14:17:45 +0600

    ready

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # best-merories-client
-# best-merories-client
+
+client : https://best-memories-ever.netlify.app/
+server : https://damp-wildwood-20776.herokuapp.com/
+
+Its a mini social full stack mern web application. One can add their best memories. He also can edit and delete his post. others can like the post if they like those.
+
+Tech used here :
+-- HTML
+-- css
+-- Javascript
+-- React
+-- material UI
+-- Axios
+-- Redux
+-- Mongodb
+-- NodeJS
+-- ExpressJS
---
+++ build/precache-manifest.0ca6d88227e031c82f5b8eed9aee8974.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a87f64af14b0bae121f8d9ef509f4ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4b2c7f1e2c24ca4338b",
+    "url": "/static/css/main.e15318f7.chunk.css"
+  },
+  {
+    "revision": "20ae1808e1c9af56956c",
+    "url": "/static/js/2.bfb52602.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.bfb52602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4b2c7f1e2c24ca4338b",
+    "url": "/static/js/main.326cee1f.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
